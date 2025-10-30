56f8617524800655cc24feccba90f35f7878db15
Inyte13 <luisgardaz13@gmail.com>
2025-10-30 08:52:54 -0500

    lab13

--- laboratorio13/ejercicio13.js
+++ laboratorio13/ejercicio13.js
@@ -1,11 +1,10 @@
 function combinarCatalogos(tienda1, tienda2){
-  let union={...tienda1,tienda2}
-  let arr1=Object.entries(tienda1)
-  let arr2=Object.entries(tienda2)
-  
-
-  
-
+  let union={}
+  for(let key of new Set([...Object.keys(tienda1),...Object.keys(tienda2)])){
+    union[key]=Math.max(tienda1[key]||0,tienda2[key]||0).toFixed(2)
+  }
+  return union
 }
 let tiendaA={"laptop":3500,"mouse":100.35,"teclado":250.9}
 let tiendaB={"mouse":95.2,"monitor":720.457,"teclado":260.1}
+console.log(combinarCatalogos(tiendaA,tiendaB))
---
+++ laboratorio13/ejercicio14.js
@@ -0,0 +1,12 @@
+function gestionarEmpleados(arr){
+  let map=new Map()
+
+
+
+}
+const listaEmpleados=[
+  {id: 1, nombre:"Juan", area: "Ventas", salario: 2400},
+  {id: 2, nombre:"Marta", area: "Ventas", salario: 2600},
+  {id: 3, nombre:"Luis", area: "TI", salario: 4000},
+  {id: 4, nombre:"Ana", area: "Recursos Humanos", salario: 3000}
+]
